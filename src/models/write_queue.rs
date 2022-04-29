56c297fcfb2135683bdee651dab87e2ea6bcc533
Edward Wong <edwardlm.wong@gmail.com>
2022-04-28 20:34:03 -0400

    Write txn per client into files. In dev build with 3 worker threads, processing 1M lines per block. About 22M lines processed..
    :
    :
    Time elapsed is: 169.622µs
    worker 1 wrote --> block: 21, client: "4636", num rows: 22
    Time elapsed is: 342.093µs
    worker 1 wrote --> block: 21, client: "112006", num rows: 12
    Time elapsed is: 206.997µs
    worker 1 wrote --> block: 21, client: "121403", num rows: 1
    Time elapsed is: 529.152µs
    worker 1 wrote --> block: 21, client: "6985", num rows: 33
    Time elapsed is: 274.274µs
    worker 1 is shutdown
    worker 0 is shutdown
    worker 2 is shutdown
    Time elapsed is: 291.89574736s

--- src/models/write_queue.rs
+++ src/models/write_queue.rs
@@ -1,7 +1,7 @@
 use crossbeam_channel::{unbounded, Receiver};
 use csv::ByteRecord;
-// use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
+use std::fs;
 use std::sync::{Arc, Mutex, MutexGuard};
 use std::thread;
 use std::time::Duration;
@@ -10,8 +10,9 @@ use super::error::AppError;
 use super::timer::Timer;
 
 const PATH: &str = "model/write_queue";
-// const TXN_DIR: &str = "parse/txns";
-// const FN_WRITE_CSV: &str = "write_client_txns";
+const TX_DIR: &str = "data/transactions";
+const FN_WRITE_TX: &str = "write_tx";
+const EXT: &str = ".csv";
 
 const MTX_NUM_TRIES: u8 = 3;
 const MTX_SLEEP_DURATION: u64 = 20;
@@ -135,22 +136,6 @@ where
                         drop(q);
                         drop(mq);
                         let _ = WriteQueue::write_tx(i, &entry);
-                        // for (k, v) in entry.map() {
-                        //     let timer = Timer::start();
-                        //     let client = String::from_utf8(k.clone()).unwrap();
-                        //     let mut rows = 0;
-                        //     for _ in v {
-                        //         rows += 1;
-                        //     }
-                        //     println!(
-                        //         "worker {} wrote --> block: {}, client: {:?}, num rows: {}",
-                        //         i,
-                        //         entry.block(),
-                        //         client,
-                        //         rows
-                        //     );
-                        //     timer.stop();
-                        // }
                     }
                 }
 
@@ -175,110 +160,49 @@ where
     fn write_tx(i: u8, entry: &T) -> Result<(), AppError> {
         for (k, v) in entry.map() {
             let timer = Timer::start();
-            let client = String::from_utf8(k.clone()).unwrap();
+
+            let client_id_str = String::from_utf8(k.to_vec())
+                .map_err(|e| AppError::new(PATH, FN_WRITE_TX, "00", &e.to_string()))?;
+
+            let dir_path = [TX_DIR, "/cid_", &client_id_str].join("");
+            fs::create_dir_all(&dir_path)
+                .map_err(|e| AppError::new(PATH, FN_WRITE_TX, "01", &e.to_string()))?;
+
+            let file_path = [
+                &dir_path,
+                "/cid_",
+                &client_id_str,
+                "_blk_",
+                &entry.block().to_string(),
+                EXT,
+            ]
+            .join("");
+
+            let mut wtr = csv::Writer::from_path(&file_path)
+                .map_err(|e| AppError::new(PATH, FN_WRITE_TX, "02", &e.to_string()))?;
+            wtr.write_byte_record(&ByteRecord::from(
+                &["userId", "movieId", "rating", "timestamp"][..],
+            ))
+            .map_err(|e| AppError::new(PATH, FN_WRITE_TX, "03", &e.to_string()))?;
+
             let mut rows = 0;
-            for _ in v {
+            for record in v {
+                wtr.write_byte_record(record)
+                    .map_err(|e| AppError::new(PATH, FN_WRITE_TX, "04", &e.to_string()))?;
                 rows += 1;
             }
+            wtr.flush()
+                .map_err(|e| AppError::new(PATH, FN_WRITE_TX, "05", &e.to_string()))?;
             println!(
                 "worker {} wrote --> block: {}, client: {:?}, num rows: {}",
                 i,
                 entry.block(),
-                client,
+                client_id_str,
                 rows
             );
             timer.stop();
         }
 
-        // let client_str = ["client", &client_id.to_string()].join("_");
-        // let dir_path = [OUTPUT_DIR, &client_str].join("/");
-        // fs::create_dir_all(&dir_path)
-        //     .map_err(|e| AppError::new(PATH, FN_WRITE_CSV, "00", &e.to_string()))?;
-
-        // let file_path = [
-        //     &dir_path,
-        //     "/",
-        //     &client_str,
-        //     "_block_",
-        //     &block_id.to_string(),
-        //     ".csv",
-        // ]
-        // .join("");
-
-        // let mut wtr = csv::Writer::from_path(&file_path)
-        //     .map_err(|e| AppError::new(PATH, FN_WRITE_CSV, "01", &e.to_string()))?;
-
-        // wtr.write_record(&["userId", "movieId", "rating", "timestamp"])
-        //     .map_err(|e| AppError::new(PATH, FN_WRITE_CSV, "02", &e.to_string()))?;
-
-        // for row in tx_rows {
-        //     wtr.write_record(&[
-        //         &row.type_id.to_string(),
-        //         &row.client_id.to_string(),
-        //         &row.tx_id.to_string(),
-        //         &row.amount.unwrap_or(0).to_string(),
-        //     ])
-        //     .map_err(|e| AppError::new(PATH, FN_WRITE_CSV, "03", &e.to_string()))?;
-        // }
-
-        // wtr.flush()
-        //     .map_err(|e| AppError::new(PATH, FN_WRITE_CSV, "04", &e.to_string()))?;
-
         Ok(())
     }
 }
-
-//     pub fn write_client_txns(
-//         &self,
-//         client_id: &u32,
-//         block_id: &usize,
-//         tx_rows: &Vec<TxRow>,
-//     ) -> Result<(), AppError> {
-//         let client_str = ["client", &client_id.to_string()].join("_");
-//         let dir_path = [OUTPUT_DIR, &client_str].join("/");
-//         fs::create_dir_all(&dir_path)
-//             .map_err(|e| AppError::new(PATH, FN_WRITE_CSV, "00", &e.to_string()))?;
-
-//         let file_path = [
-//             &dir_path,
-//             "/",
-//             &client_str,
-//             "_block_",
-//             &block_id.to_string(),
-//             ".csv",
-//         ]
-//         .join("");
-
-//         let mut wtr = csv::Writer::from_path(&file_path)
-//             .map_err(|e| AppError::new(PATH, FN_WRITE_CSV, "01", &e.to_string()))?;
-
-//         wtr.write_record(&["userId", "movieId", "rating", "timestamp"])
-//             .map_err(|e| AppError::new(PATH, FN_WRITE_CSV, "02", &e.to_string()))?;
-
-//         for row in tx_rows {
-//             wtr.write_record(&[
-//                 &row.type_id.to_string(),
-//                 &row.client_id.to_string(),
-//                 &row.tx_id.to_string(),
-//                 &row.amount.unwrap_or(0).to_string(),
-//             ])
-//             .map_err(|e| AppError::new(PATH, FN_WRITE_CSV, "03", &e.to_string()))?;
-//         }
-
-//         wtr.flush()
-//             .map_err(|e| AppError::new(PATH, FN_WRITE_CSV, "04", &e.to_string()))?;
-
-//         Ok(())
-//     }
-
-// #[derive(Debug, Deserialize, Serialize, Clone)]
-// pub struct TxRow<'a> {
-//     #[serde(rename(deserialize = "userId", serialize = "userId"))]
-//     pub type_id: &'a str,
-//     #[serde(rename(deserialize = "movieId", serialize = "movieId"))]
-//     pub client_id: u32,
-//     #[serde(rename(deserialize = "rating", serialize = "rating"))]
-//     pub tx_id: f32,
-//     #[serde(rename(deserialize = "timestamp", serialize = "timestamp"))]
-//     pub amount: Option<u32>,
-// }
