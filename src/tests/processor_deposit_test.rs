6fbaf25a6c88a77b23d2bab817be2b7cbf81bd89
Edward Wong <edwardlm.wong@gmail.com>
2022-05-06 11:51:13 -0400

    Multi csv deposit test.

--- src/tests/processor_deposit_test.rs
+++ src/tests/processor_deposit_test.rs
@@ -42,3 +42,47 @@ fn process_deposit_test() {
     TestHelper::remove_file(&[ACCOUNT_DIR, "3.csv"].join("/"));
     TestHelper::remove_file(&[ACCOUNT_DIR, "4.csv"].join("/"));
 }
+
+
+#[test]
+fn process_deposit_multi_test() {
+    // --------- //
+    // input csv //
+    // --------- //
+
+    // type,client,tx,amount
+    // deposit,22,1,1
+    // deposit,22,2,1
+    // deposit,22,3,1
+    // deposit,22,4,1
+
+    // type,client,tx,amount
+    // deposit,22,5,2
+    // deposit,22,6,2
+    // deposit,22,7,2
+    // deposit,22,8,2
+    
+    let client_id = 22;
+    let result = Processor::new("src/tests/csv/deposit_multi_0.csv");
+    assert!(result.is_ok());
+
+    let p = result.unwrap();
+    let result = p.process_data(false);
+    assert!(result.is_ok());
+
+    let result = Processor::new("src/tests/csv/deposit_multi_1.csv");
+    assert!(result.is_ok());
+
+    let p = result.unwrap();
+    let result = p.process_data(false);
+    assert!(result.is_ok());
+
+    let account = Account::new(client_id, ACCOUNT_DIR);
+    assert_eq!(account.client_id, client_id);
+    assert_eq!(account.available, Decimal::new(12, 0));
+    assert_eq!(account.held, Decimal::new(0, 0));
+    assert_eq!(account.total, Decimal::new(12, 0));
+    assert!(!account.locked);
+
+    TestHelper::remove_file(&[ACCOUNT_DIR, "/", &client_id.to_string(), ".csv"].join(""));
+}
